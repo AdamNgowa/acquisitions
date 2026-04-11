3f9dbe4d461f4d81eb3ae3b786957e86051dae70
Adam Masha <adamngowa3@gmail.com>
2026-04-11 17:30:22 +0300

    Set up neon postgress w/ Drizzle

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from acquisitions");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from acquisitions');
 });
 
 export default app;
